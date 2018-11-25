3f290537441a31a3140e4bfbd06dfa6f8bee7306
angelcoder <31827866+angelcoder@users.noreply.github.com>
2018-11-25 17:58:11 +0200

    Add files via upload
    jarvis algo added

---
+++ Jarvis.hpp
@@ -0,0 +1,18 @@
+//
+//  Jarvis.hpp
+//  lab3
+//
+//  Created by Anhelina Lohvina on 25.11.2018.
+//  Copyright © 2018 Anhelina Lohvina. All rights reserved.
+//
+
+#ifndef Jarvis_hpp
+#define Jarvis_hpp
+
+#include <stdio.h>
+struct Point
+{
+    int x, y;
+};
+void convexHull(Point points[], int n);
+#endif /* Jarvis_hpp */
