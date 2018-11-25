67730f6a5a5f596fb84bb0c858051fb10d3b728b
angelcoder <31827866+angelcoder@users.noreply.github.com>
2018-11-25 18:03:50 +0200

    Add files via upload

---
+++ divideandconqueralgo.h
@@ -0,0 +1,11 @@
+#ifndef DIVIDEANDCONQUERALGO_H
+#define DIVIDEANDCONQUERALGO_H
+
+
+#include <set>
+#include <iostream>
+using namespace std;
+#define Pair pair<int, int> // Pair is a pare of two integers
+
+void printHull(Pair a[], int n);
+#endif // DIVIDEANDCONQUERALGO_H
---
+++ javis.h
@@ -0,0 +1,10 @@
+#ifndef JAVIS_H
+#define JAVIS_H
+
+struct Point
+{
+    int x, y;
+};
+void convexHull(Point points[], int n);
+
+#endif // JAVIS_H
