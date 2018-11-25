7f16330a61e374c06fc90d3a2ead9d74da1c9572
angelcoder <31827866+angelcoder@users.noreply.github.com>
2018-11-25 19:05:22 +0200

    Update main.cpp
    graham added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "divideandconqueralgo.h"
-#include "javis.h"
+//#include "javis.h"
+#include "graham.h"
 #include <iostream>
 #include <algorithm>
 #include <set>
@@ -7,27 +8,36 @@
 using namespace std;
 
 int main(){
-    int main(){
-        int method;
-        cin >> method;
-        switch (method) {
-            case 4:{
-                cout <<"javis method\n";
-                Point points[] = {{0, 3}, {2, 2}, {1, 1}, {2, 1},
-                    {3, 0}, {0, 0}, {3, 3}};
-                int n4 = sizeof(points)/sizeof(points[0]);
-                convexHull(points, n4);
-                break;
-            }
-            case 6:{
-                cout <<"Divide and counquer method\n";
-                Pair a[ ] =  {{0, 3}, {2, 2}, {1, 1}, {2, 1},{3, 0}, {0, 0}, {3, 3}};
-                int n6 = sizeof(a)/sizeof(a[0]);
-                printHull(a, n6);
-                break;
-            }
-        }
-        return 0;
+    int method;
+    cin >> method;
+    //THERE SHOULD BE A VECTOR OF FUNCTIONS SOON
+    switch (method) {
+    /*
+    case 4:{
+        cout <<"javis method\n";
+        Point points[] = {{0, 3}, {2, 2}, {1, 1}, {2, 1},
+            {3, 0}, {0, 0}, {3, 3}};
+        int n4 = sizeof(points)/sizeof(points[0]);
+        convexHull(points, n4);
+        break;
     }
+    */
+    case 5:{
+        Point pointsG[] = {{0, 3}, {1, 1}, {2, 2}, {4, 4},
+                          {0, 0}, {1, 2}, {3, 1}, {3, 3}};
+        int n5 = sizeof(pointsG)/sizeof(pointsG[0]);
+        convexHullG(pointsG, n5);
+        break;
+    }
+    case 6:{
+        cout <<"Divide and counquer method\n";
+        Pair a[ ] =  {{0, 3}, {2, 2}, {1, 1}, {2, 1},{3, 0}, {0, 0}, {3, 3}};
+        int n6 = sizeof(a)/sizeof(a[0]);
+        printHull(a, n6);
+        break;
+    }
+    }
+    return 0;
 }
+
 
