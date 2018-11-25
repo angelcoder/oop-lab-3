57e2fc8751ee0fe34046625632f616e0034886f6
Anhelina Lohvina <lohvina.anhelina98.shostka@gmail.com>
2018-11-25 18:30:29 +0200

    switch added

--- main.cpp
+++ main.cpp
@@ -7,10 +7,27 @@
 using namespace std;
 
 int main(){
-    Point points[] = {{0, 3}, {2, 2}, {1, 1}, {2, 1},
-        {3, 0}, {0, 0}, {3, 3}};
-    int n = sizeof(points)/sizeof(points[0]);
-    convexHull(points, n);
-    return 0;
+    int main(){
+        int method;
+        cin >> method;
+        switch (method) {
+            case 4:{
+                cout <<"javis method\n";
+                Point points[] = {{0, 3}, {2, 2}, {1, 1}, {2, 1},
+                    {3, 0}, {0, 0}, {3, 3}};
+                int n4 = sizeof(points)/sizeof(points[0]);
+                convexHull(points, n4);
+                break;
+            }
+            case 6:{
+                cout <<"Divide and counquer method\n";
+                Pair a[ ] =  {{0, 3}, {2, 2}, {1, 1}, {2, 1},{3, 0}, {0, 0}, {3, 3}};
+                int n6 = sizeof(a)/sizeof(a[0]);
+                printHull(a, n6);
+                break;
+            }
+        }
+        return 0;
+    }
 }
 
