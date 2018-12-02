14eaa00165faaebec1dab82b10e329119a322f3a
angelcoder <31827866+angelcoder@users.noreply.github.com>
2018-12-02 20:29:36 +0200

    convexHullG returns vector<Point> type

--- graham.h
+++ graham.h
@@ -1,11 +1,10 @@
 #ifndef GRAHAM_H
 #define GRAHAM_H
 
-struct Point
-{
-    int x, y;
-};
+#include <stdio.h>
+#include "Point.h"
+#include <vector>
 
-void convexHullG(Point points[], int n);
+std::vector<Point>  convexHullG(Point points[], int n);
 
 #endif // GRAHAM_H
