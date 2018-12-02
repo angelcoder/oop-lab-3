f47a87b5fcb81bb148488456271e3776755c93a7
angelcoder <31827866+angelcoder@users.noreply.github.com>
2018-12-02 20:23:12 +0200

    convexHullJ returns vector<Point> type

--- javis.h
+++ javis.h
@@ -1,10 +1,9 @@
 #ifndef JAVIS_H
 #define JAVIS_H
 
-struct Point
-{
-    int x, y;
-};
-void convexHull(Point points[], int n);
+#include "Point.h"
+#include <vector>
+
+std::vector<Point> convexHullJ(Point points[], int n);
 
 #endif // JAVIS_H
