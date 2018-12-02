7c1d00816e2a97b835cf6b2a66c82a37b91d741b
angelcoder <31827866+angelcoder@users.noreply.github.com>
2018-12-02 20:44:39 +0200

    class Point file added

---
+++ Point.h
@@ -0,0 +1,17 @@
+//
+//  Point.h
+//  lab3
+//
+//  Created by Anhelina Lohvina on 02.12.2018.
+//  Copyright © 2018 Anhelina Lohvina. All rights reserved.
+//
+
+#ifndef Point_h
+#define Point_h
+
+class Point{
+public:
+    int x, y;
+};
+
+#endif /* Point_h */
