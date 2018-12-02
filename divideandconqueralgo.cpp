26ad931eb9b9e1c075aa79344033b703c8cbdef3
angelcoder <31827866+angelcoder@users.noreply.github.com>
2018-12-02 20:13:35 +0200

    printHull returns vector<Point> type

--- divideandconqueralgo.cpp
+++ divideandconqueralgo.cpp
@@ -1,30 +1,30 @@
-#include "divideandconqueralgo.h"
+#include "DivideAndConquerAlgo.hpp"
+#include <vector>
 
-#include <set>
 using namespace std;
-
-set<Pair> hull;  //points of convex hull
-
-int findSide(Pair p1, Pair p2, Pair p){
-    int val = (p.second - p1.second) * (p2.first - p1.first) -
-    (p2.second - p1.second) * (p.first - p1.first);
-
+vector<Point> hull;  //points of convex hull
+bool operator<(Point p1, Point p2) {
+    if (p1.y == p2.y) return p1.x < p2.x;
+    return (p1.y < p2.y);
+}
+bool operator==(Point p1, Point p2) {
+    return (p1.x == p2.x && p1.y == p2.y);
+}
+int findSide(Point p1, Point p2, Point p){
+    int val = (p.y - p1.y) * (p2.x - p1.x) -
+    (p2.y - p1.y) * (p.x - p1.x);
     if (val > 0) return 1;
     if (val < 0) return -1;
     return 0;
 }
-
-int lineDist(Pair p1, Pair p2, Pair p){
-    return abs ((p.second - p1.second) * (p2.first - p1.first) -
-                (p2.second - p1.second) * (p.first - p1.first));
+int lineDist(Point p1, Point p2, Point p){
+    return abs ((p.y - p1.y) * (p2.x - p1.x) -
+                (p2.y - p1.y) * (p.x - p1.x));
 }
 
-void quickHull(Pair a[], int n, Pair p1, Pair p2, int side){
+void convexHullRec(Point a[], int n, Point p1, Point p2, int side){
     int ind = -1;
     int max_dist = 0;
-
-    // finding the point with maximum distance
-    // from L and also on the specified side of L.
     for (int i=0; i<n; i++){
         int temp = lineDist(p1, p2, a[i]);
         if (findSide(p1, p2, a[i]) == side && temp > max_dist){
@@ -32,37 +32,26 @@ void quickHull(Pair a[], int n, Pair p1, Pair p2, int side){
             max_dist = temp;
         }
     }
-
     if (ind == -1){
-        hull.insert(p1);
-        hull.insert(p2);
+        hull.push_back(p1);
+        hull.push_back(p2);
+        sort(hull.begin(), hull.end());
         return;
     }
-
-    // Recur for the two parts divided by a[ind]
-    quickHull(a, n, a[ind], p1, -findSide(a[ind], p1, p2));
-    quickHull(a, n, a[ind], p2, -findSide(a[ind], p2, p1));
+    convexHullRec(a, n, a[ind], p1, -findSide(a[ind], p1, p2));
+    convexHullRec(a, n, a[ind], p2, -findSide(a[ind], p2, p1));
 }
 
-void printHull(Pair a[], int n){
-    // a[i].second -> y-coordinate of the ith point
-    if (n < 3) {
-        cout << "Convex hull not possible\n";
-        return;
-    }
-    
+vector<Point> printHull(Point a[], int n){
     int min_x = 0, max_x = 0;
     for (int i=1; i<n; i++){
-        if (a[i].first < a[min_x].first) min_x = i;
-        if (a[i].first > a[max_x].first) max_x = i;
+        if (a[i].x < a[min_x].x) min_x = i;
+        if (a[i].x > a[max_x].x) max_x = i;
     }
-    quickHull(a, n, a[min_x], a[max_x], 1);
-    quickHull(a, n, a[min_x], a[max_x], -1);
-
+    convexHullRec(a, n, a[min_x], a[max_x], 1);
+    convexHullRec(a, n, a[min_x], a[max_x], -1);
     cout << "The points in Convex Hull are:\n";
-    while (!hull.empty()){
-        cout << "(" <<( *hull.begin()).first << ", "
-        << (*hull.begin()).second << ") ";
-        hull.erase(hull.begin());
-    }
+    sort(hull.begin(), hull.end());
+    hull.erase(unique( hull.begin(), hull.end() ), hull.end());
+    return hull;
 }
